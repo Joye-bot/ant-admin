f470a836b7cc93936233876ac7c6785f1d023a54
nndmw <sllfdspfe735@gmail.com>
2021-11-06 10:24:09 +0800

    add dependencies config

--- src/main.js
+++ src/main.js
@@ -3,7 +3,17 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import { Button, Layout, Menu, Icon, Avatar, Dropdown } from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.less'
+Vue.use(Button)
+Vue.use(Layout)
+Vue.use(Menu)
+Vue.use(Icon)
+Vue.use(Avatar)
+Vue.use(Dropdown)
+
 Vue.config.productionTip = false
+Vue.config.devtools = false
 
 new Vue({
   router,
